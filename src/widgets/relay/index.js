7cab7dde9bfe356d776cc75f77ec43ab71a2a4bb
Amory Galili <amorygalili@gmail.com>
2020-01-13 15:45:19 -0500

    Updated structure of relay widget. Fixed bug where you couldn't update it without a source

--- src/widgets/relay/index.js
+++ src/widgets/relay/index.js
@@ -1,10 +1,71 @@
-const RelayWidget = require('./relay');
-const image = require('./relay.png');
+const { LitElement, html, css } = dashboard.lit;
+
+class RelayWidget extends LitElement {
+
+  static get styles() {
+    return css`
+      :host {
+        display: inline-block;
+        font-size: 15px;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+      }
+
+      vaadin-button {
+        border-radius: 0;
+        margin: 0;
+        flex: 1;
+        font-size: inherit;
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      value: { type: String, attribute: false }
+    };
+  }
+
+  constructor() {
+    super();
+    this.buttons = ['Off', 'On', 'Forward', 'Reverse'];
+    this.value = 'Off';
+  }
+
+  setValue(value) {
+    if ('Value' in this.table) {
+      NetworkTables.putValue(this.ntRoot + 'Value', value);
+    }
+    else {
+      this.value = value;
+    }
+  }
+
+  updated() {
+    if ('Value' in this.table) {
+      this.value = this.table.Value;
+    }
+  }
+
+  render() {
+    return html`   
+      ${this.buttons.map(button => html`
+        <vaadin-button 
+          theme="contrast ${this.value === button ? 'primary' : ''}" 
+          @click="${() => this.setValue(button)}"
+        >
+          ${button}
+        </vaadin-button>
+      `)}
+    `;
+  }
+}
 
 dashboard.registerWidget('relay-widget', {
   class: RelayWidget,
   label: 'Relay',
   category: 'Basic',
   acceptedTypes: ['Relay'],
-  image
+  image: require('./relay.png')
 });
