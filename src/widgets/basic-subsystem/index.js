97501e06b4d9eb06e39833d1a6e3f5fc4618a880
Amory Galili <amorygalili@gmail.com>
2020-01-13 12:19:30 -0500

    Removed widget props for boolean-box and added properties to the class instead. Updated the widget file structure. Updated the file structure for other widgets as well

--- src/widgets/basic-subsystem/index.js
+++ src/widgets/basic-subsystem/index.js
@@ -1,10 +1,37 @@
-const BasicSubsystem = require('./basic-subsystem');
-const image = require('./basic-subsystem.png');
+const { LitElement, html, css } = dashboard.lit;
+
+class BasicSubsystem extends LitElement {
+
+  static get styles() {
+    return css`
+      :host {
+        text-align: left;
+        font-weight: normal;
+        display: inline-block;
+      }
+
+      p {
+        margin: 0;
+      }
+
+      p:first-child {
+        margin-bottom: 5px;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <p>Default command: ${this.table['.default'] || 'None'}</p>
+      <p>Current command: ${this.table['.command'] || 'None'}</p>
+    `;
+  }
+}
 
 dashboard.registerWidget('basic-subsystem', {
   class: BasicSubsystem,
   label: 'Basic Subsystem',
   category: 'Basic',
   acceptedTypes: ['Subsystem'],
-  image
+  image: require('./basic-subsystem.png')
 });
--- src/widgets/boolean-box/index.js
+++ src/widgets/boolean-box/index.js
@@ -1,18 +1,67 @@
-const BooleanBox = require('./boolean-box');
-const BooleanBoxProps = require('./boolean-box-props')
-const image = require('./boolean-box.png');
+const { LitElement, html, css } = dashboard.lit;
+
+class BooleanBox extends LitElement {
+
+  static get styles() {
+    return css`
+      :host { 
+        display: block; 
+        width: 100px;
+        height: 100px;
+      }
+
+      [part=box] {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        flex-wrap: wrap;
+        background-color: var(--box-color);
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      defaultColor: { type: String, attribute: 'default-color' },
+      trueColor: { type: String, attribute: 'true-color' },
+      falseColor: { type: String, attribute: 'false-color' }
+    };
+  }
+
+  constructor() {
+    super();
+    this.defaultColor = 'black'
+    this.trueColor = 'green';
+    this.falseColor = 'red';
+  }
+
+  updated() {
+    const backgroundNode = this.shadowRoot.querySelector('[part=box]');
+    let backgroundColor = this.defaultColor;
+    if (this.table === true) {
+      backgroundColor = this.trueColor;
+    }
+    else if (this.table === false) {
+      backgroundColor = this.falseColor;
+    }
+    backgroundNode.style.setProperty('--box-color', backgroundColor);
+  }
+
+  render() {
+    return html`
+      <div part="box">
+        <slot></slot>
+      </div>
+    `;
+  }
+}
 
 dashboard.registerWidget('boolean-box', {
   class: BooleanBox,
   label: 'Boolean Box',
   category: 'Basic',
   acceptedTypes: ['boolean'],
-  image,
-  properties: {
-    class: BooleanBoxProps,
-    defaults: {
-      colorWhenTrue: '#00ff00',
-      colorWhenFalse: '#ff0000'
-    }
-  }
+  image: require('./boolean-box.png')
 });
