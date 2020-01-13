89f9558c292ebdde78ebc7a5ef78e46c8fd28b47
Amory Galili <amorygalili@gmail.com>
2020-01-13 16:01:52 -0500

    Updated the file structure of some widgets

--- src/widgets/toggle-button/index.js
+++ src/widgets/toggle-button/index.js
@@ -1,10 +1,68 @@
-const ToggleButton = require('./toggle-button');
-const image = require('./toggle-button.png');
+const { LitElement, html, css } = dashboard.lit;
+
+class ToggleButton extends LitElement {
+
+  static get styles() {
+    return css`
+      :host {
+        display: inline-block;
+        width: 100px;
+        height: 50px;
+      }
+
+      [part=button] {
+        width: 100%;
+        height: 100%;
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      value: { type: Boolean },
+      theme: { type: String }
+    }
+  }
+
+  constructor() {
+    super();
+    this.value = false;
+    this.theme = 'contrast';
+  }
+
+  updated() {
+    if (this.ntRoot) {
+      this.value = this.table;
+    }
+  }
+
+  onClick() {
+    if (typeof this.table === 'boolean') {
+      NetworkTables.putValue(this.ntRoot, !this.value);
+    }
+    else {
+      this.value = !this.value;
+    }
+  };
+
+  render() {
+    return html`   
+      <vaadin-button 
+        theme="${this.theme} ${this.value ? 'primary' : ''}"
+        part="button"
+        @click="${this.onClick}"
+      >
+        <slot></slot>
+      </vaadin-button>
+    `;
+  }
+}
+
 
 dashboard.registerWidget('toggle-button', {
   class: ToggleButton,
   label: 'Toggle Button',
   category: 'Basic',
   acceptedTypes: ['boolean'],
-  image
+  image: require('./toggle-button.png')
 });
